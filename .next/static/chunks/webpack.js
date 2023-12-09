4d5f770debb4ed5352668d6205b99ce0e65ed0a8
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-12-09 09:36:37 +0330

    Linking Between Pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a326519960e87b6b"; }
+/******/ 		__webpack_require__.h = function() { return "3b2c6a1df52164d1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,18 @@
+import Link from "next/link";
 function NewsPage() {
-  return <h1>The news Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="news/nextjs-is-a-great-framework">
+            NextJS Is a Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
